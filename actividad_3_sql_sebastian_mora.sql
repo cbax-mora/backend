d3eea31de167368c64e583e310d7275bab8be7c2
cbax-mora <cbax0619@gmail.com>
2022-06-14 02:03:12 -0500

    actividad 3 base de datos

--- actividad_3_sql_sebastian_mora.sql
+++ actividad_3_sql_sebastian_mora.sql
@@ -16,51 +16,56 @@
 
 
 -- Volcando estructura de base de datos para tienda_deportiva_el_podio
-DROP DATABASE IF EXISTS `tienda_deportiva_el_podio`;
 CREATE DATABASE IF NOT EXISTS `tienda_deportiva_el_podio` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `tienda_deportiva_el_podio`;
 
 -- Volcando estructura para tabla tienda_deportiva_el_podio.cumtomers
-DROP TABLE IF EXISTS `cumtomers`;
 CREATE TABLE IF NOT EXISTS `cumtomers` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `name_customers` varchar(50) NOT NULL DEFAULT '0',
   `customers_premium` bit(1) NOT NULL DEFAULT b'1',
-  `phone_customers` varchar(10) NOT NULL DEFAULT '',
+  `phone_customers` varchar(10) NOT NULL DEFAULT '3003003232',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla tienda_deportiva_el_podio.cumtomers: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla tienda_deportiva_el_podio.cumtomers: ~3 rows (aproximadamente)
 DELETE FROM `cumtomers`;
+INSERT INTO `cumtomers` (`id`, `name_customers`, `customers_premium`, `phone_customers`) VALUES
+	(1, 'kevin', b'1', '3003003232'),
+	(2, 'omar', b'0', '3003003231'),
+	(3, 'nick', b'1', '3003003236');
 
 -- Volcando estructura para tabla tienda_deportiva_el_podio.paymet_method
-DROP TABLE IF EXISTS `paymet_method`;
 CREATE TABLE IF NOT EXISTS `paymet_method` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cash` float NOT NULL DEFAULT 0,
-  `check` float NOT NULL DEFAULT 0,
-  `tdc_amex` float NOT NULL DEFAULT 0,
+  `method` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla tienda_deportiva_el_podio.paymet_method: ~0 rows (aproximadamente)
 DELETE FROM `paymet_method`;
+INSERT INTO `paymet_method` (`id`, `method`) VALUES
+	(1, 'cash'),
+	(2, 'tdc_amex'),
+	(3, 'check');
 
 -- Volcando estructura para tabla tienda_deportiva_el_podio.products
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `name_product` varchar(50) NOT NULL DEFAULT '0',
   `measure_product` varchar(50) NOT NULL DEFAULT '0',
   `product_import` bit(1) NOT NULL DEFAULT b'1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla tienda_deportiva_el_podio.products: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla tienda_deportiva_el_podio.products: ~3 rows (aproximadamente)
 DELETE FROM `products`;
+INSERT INTO `products` (`id`, `name_product`, `measure_product`, `product_import`) VALUES
+	(1, 'ball', '30cm', b'1'),
+	(2, 'gloves', '15cm', b'1'),
+	(3, 't-shirt', 'xl', b'1');
 
 -- Volcando estructura para tabla tienda_deportiva_el_podio.selling
-DROP TABLE IF EXISTS `selling`;
 CREATE TABLE IF NOT EXISTS `selling` (
   ` id` smallint(6) NOT NULL AUTO_INCREMENT,
   `sale_value` float NOT NULL DEFAULT 0,
@@ -70,10 +75,14 @@ CREATE TABLE IF NOT EXISTS `selling` (
   `id_product` smallint(6) NOT NULL DEFAULT 0,
   `id_paymet_method` smallint(6) NOT NULL DEFAULT 0,
   PRIMARY KEY (` id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla tienda_deportiva_el_podio.selling: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla tienda_deportiva_el_podio.selling: ~2 rows (aproximadamente)
 DELETE FROM `selling`;
+INSERT INTO `selling` (` id`, `sale_value`, `nro_sale`, `date`, `id_customers`, `id_product`, `id_paymet_method`) VALUES
+	(1, 20000, 1, '2022-12-05', 1, 1, 1),
+	(2, 400000, 2, '2022-06-14', 2, 2, 2),
+	(3, 500000, 3, '2022-06-14', 3, 3, 3);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
